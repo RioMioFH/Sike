9ad38af919c02f61e33a1251bbcf55b9ea63b355
RioMioFH <fhb251250@fh-vie.ac.at>
2025-12-25 22:43:18 +0100

    Squashed commit of the following:
    commit f72c08132991fb75693e1549a25b0310042f1ee8
    Author: RioMioFH <fhb251250@fh-vie.ac.at>
    Date:   Thu Dec 18 01:58:43 2025 +0100
    
        Add basic game over screen and restart flow
    
        - Show game over UI on player death
        - Restart level on input
        - Remove immediate respawn on kill zone
    
    commit cada4ea15358ff7ec32b863c742ec69aa974f4b0
    Author: RioMioFH <fhb251250@fh-vie.ac.at>
    Date:   Wed Dec 17 23:40:01 2025 +0100
    
        Implement death and respawn system (GameManager, KillZone) and add comments to PlayerController

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    // Unity method called when another object enters the trigger collider
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object that entered is the player
+        if (other.CompareTag("Player"))
+        {
+            // Tell the GameManager to respawn the player
+            FindAnyObjectByType<GameOverUI>().Show();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,52 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Rigidbody of the player used for movement and jumping
     private Rigidbody2D playerRigidbody;
 
+    // Horizontal movement speed
     [SerializeField] private float moveSpeed = 6f;
+    // Jump force applied to the player
     [SerializeField] private float jumpSpeed = 12f;
-
+    // Position used to check if the player is on the ground
     [SerializeField] private Transform groundCheck;
+    // Radius of the ground check circle
     [SerializeField] private float groundCheckRadius = 0.15f;
+    // Layer that represents the ground
     [SerializeField] private LayerMask groundLayer;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Get Rigidbody2D component from the player object
         this.playerRigidbody = this.gameObject.GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Read horizontal input (A/D or left/right arrows)
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // Apply horizontal movement
         this.playerRigidbody.linearVelocityX = horizontalInput * this.moveSpeed;
 
+        // Check if any jump key was pressed
         bool jumpPressed = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
 
+        // Jump only if jump key is pressed and player is on the ground
         if (jumpPressed && this.IsGrounded())
         {
             this.playerRigidbody.linearVelocityY = this.jumpSpeed;
         }
     }
 
+
+    // Method that checks if the player is touching the ground
     private bool IsGrounded()
     {
+        // Overlap circle at groundCheck position to detect ground
         return Physics2D.OverlapCircle(this.groundCheck.position, this.groundCheckRadius, this.groundLayer);
     }
 }
