1a1b0f23127f68cf3c2f98ac8aeb189ff0e9bc05
Puc <christian.purcariu@gmail.com>
2026-01-25 20:51:49 +0100

    Revert "Bugfix (Level_01 Trap) & LevelEndTrigger"
    This reverts commit d244d058cf5372f46872780bff846887aef6d87d.

--- Assets/Scripts/Traps01/StartFloorTrap.cs
+++ Assets/Scripts/Traps01/StartFloorTrap.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class StartFloorTrap2 : MonoBehaviour
+public class StartFloorTrap : MonoBehaviour
 {
     // Tilemap with the floor tiles that should disappear when the trap is triggered
     [SerializeField] private Tilemap trapStartFloorTilemap;
